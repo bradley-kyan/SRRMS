6030b2f73c0baffa314107a1c89f778c9e8c7a03
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-01-03 22:23:01 +1300

    Crystal Display + Tone class
    Added prelininary code for a lcd display which currently displays UID.
    Created class method where preset tones can be set => tone.success() / tone.error()

--- UID/UID.ino
+++ UID/UID.ino
@@ -1,14 +1,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          8         // Configurable, see typical pin layout above
 
-int errorDelay = 600;
+int errorDelay = 1000;
 int successDelay = 200;
 
 String lastUid;
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
 
 void setup() {
 
@@ -37,7 +41,22 @@ Serial.begin(9600);   // Initialize serial communications with the PC
     return uidString;
   }
 
+class tones{
+  public:
+  void error(){
+      tone(2, 2600, errorDelay); //error tone to D5
+      delay(errorDelay);
+  }
+  void success(){
+      tone(2, 3300, successDelay); //Success tone to D5
+      delay(successDelay);
+      tone(2, 3400, successDelay);
+  }
+};
+
 void loop() {
+
+  tones Tones;
 	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
 	if ( ! mfrc522.PICC_IsNewCardPresent()) {
 		return;
@@ -47,24 +66,18 @@ void loop() {
     String uid = getID();
     if(lastUid == uid){ //check if current card has the same uid as last scanned card
       Serial.println("Card Already Scanned");
-      tone(5, 2600, errorDelay); //error tone to D5
-      delay(errorDelay);
-        delay(1000);
+      Tones.error();
     }
     else if(uid == "" || uid == " "){ //Check if UID is empty
-      tone(5, 2600, errorDelay);//error tone to D5
-      delay(errorDelay);
-      return;
-        delay(1000);
+      Tones.error();
     }
     else{
       Serial.print("Card detected, UID: "); Serial.println(uid);
       lastUid = uid; //save current card to local memory
-
-      tone(5, 3300, successDelay); //Success tone to D5
-      delay(successDelay);
-      tone(5, 3400, successDelay);
-      delay(1000);
+      Tones.success();
+       lcd.begin(16, 2);
+       lcd.print(uid);
     }
+    delay(1000);
   }
 }
