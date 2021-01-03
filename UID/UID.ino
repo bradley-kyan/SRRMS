787a1d4a459bafd0db9e78c4499cc3edac1c271c
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-01-03 15:32:24 +1300

    Success Tone
    Added piezo buzzer to alert the user whether or not their card was successfully scanned. Buzzer connected to D5 pin

--- UID/UID.ino
+++ UID/UID.ino
@@ -4,23 +4,25 @@
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          8         // Configurable, see typical pin layout above
 
+int errorDelay = 600;
+int successDelay = 200;
+
 String lastUid;
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
 
 Serial.begin(9600);   // Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	
+
 	SPI.begin();    // Init SPI bus
-	SPI.setClockDivider(SPI_CLOCK_DIV8);
+	SPI.setClockDivider(SPI_CLOCK_DIV16);
 
   pinMode(10, OUTPUT);
   digitalWrite(10, HIGH);
-      
+  
 	mfrc522.PCD_Init();		// Init MFRC522
   Serial.println("Successfully initalized");
-	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	delay(200);				// Optional delay. Some board do need more time after init to be ready, see Readme
 }
 
   String getID(){
@@ -45,14 +47,24 @@ void loop() {
     String uid = getID();
     if(lastUid == uid){ //check if current card has the same uid as last scanned card
       Serial.println("Card Already Scanned");
+      tone(5, 2600, errorDelay);
+      delay(errorDelay);
+        delay(1000);
     }
     else if(uid == "" || uid == " "){ //Check if UID is empty
+      tone(5, 2600, errorDelay);
+      delay(errorDelay);
       return;
+        delay(1000);
     }
     else{
       Serial.print("Card detected, UID: "); Serial.println(uid);
       lastUid = uid; //save current card to local memory
+
+      tone(5, 3300, successDelay);
+      delay(successDelay);
+      tone(5, 3400, successDelay);
+      delay(1000);
     }
   }
-  delay(2000);
 }
