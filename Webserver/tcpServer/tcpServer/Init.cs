793ce296f2d80a6e9d6f78d4b4f370376d8b3f1d
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-03-28 22:50:36 +1300

    TCP Server
    + Server data handling
    + Creation of basic application startup interface
    + Added Putty TCP for testing
    
    ~ Changed code structure of application

---
+++ Webserver/tcpServer/tcpServer/Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.IO;
+using System.Reflection;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace tcpServer
+{
+    class Init
+    {
+        const string srrmsTitle = "  ______________\n |              |\n | SRRMS Server |\n |______________| \n";
+        static string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static void startup()
+        {
+            Console.Title = "SRRMS Server";
+
+            Console.WriteLine(srrmsTitle);
+
+            if(File.Exists(currentPath + "/preferences.json"))
+            {
+                Console.WriteLine("preferences.json found\n\nPress any key to continue...");
+                Console.ReadKey();
+                startServer();
+
+            }
+            else
+            {
+                Console.WriteLine("preferences.json not found\n");
+                File.Create("preferences.json");
+                Console.WriteLine("Created preferences.json\n\nPress any key to continue...");
+                Console.ReadKey();
+                startServer();
+            }
+        }
+
+        static void startServer()
+        {
+            Console.Clear();
+            Console.WriteLine(srrmsTitle);
+            Console.WriteLine("Listening Server Started\n________________________\n\n");
+
+        }
+    }
+}
