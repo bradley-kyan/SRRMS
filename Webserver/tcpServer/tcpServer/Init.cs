cd5cae6d76869e8d37df14ef4f463cf247352f4c
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-04-01 10:10:20 +1300

    Update Startup

--- Webserver/tcpServer/tcpServer/Init.cs
+++ Webserver/tcpServer/tcpServer/Init.cs
@@ -6,43 +6,40 @@ using System.IO;
 using System.Reflection;
 using System.Drawing.Imaging;
 using System.Drawing;
+using System.Threading;
 
 namespace tcpServer
 {
     class Init
     {
         const string srrmsTitle = "  ______________\n |              |\n | SRRMS Server |\n |______________| \n";
+        const string options = "  __________________\n |   |              |\n | 1 | Setup        |\n | 2 | Start server |\n |___|______________|";
         static string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         public static void startup()
         {
             Console.Title = "SRRMS Server";
-
             Console.WriteLine(srrmsTitle);
-
-            if(File.Exists(currentPath + "/preferences.json"))
+            if (File.Exists(currentPath + "/preferences.json"))
             {
-                Console.WriteLine("preferences.json found\n\nPress any key to continue...");
-                Console.ReadKey();
-                startServer();
-
+                Console.WriteLine("preferences.json found\n\n");
+                Thread.Sleep(1000);
+                Console.Clear();
             }
             else
             {
                 Console.WriteLine("preferences.json not found\n");
                 File.Create("preferences.json");
-                Console.WriteLine("Created preferences.json\n\nPress any key to continue...");
-                Console.ReadKey();
-                startServer();
+                Console.WriteLine("Created preferences.json\n\n");
+
             }
         }
-
-        static void startServer()
-        {
-            Console.Clear();
-            Console.WriteLine(srrmsTitle);
-            Console.WriteLine("Listening Server Started\n________________________\n\n");
-
-        }
     }
+
+    class Setup : Init
+    {
+
+
+    }
+
 }
