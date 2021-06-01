61c197a0872ba728ef9e031001ad68987ebcd8ca
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-06-01 21:10:27 +1200

    Update SRRMS Creation.sql
    + Add unique constraints and foreign key error fixes

--- Queries/SRRMS Creation.sql
+++ Queries/SRRMS Creation.sql
@@ -1,6 +1,20 @@
 CREATE DATABASE SRRMS_DB
+GO
 
-CREATE TABLE Student_Info(
+CREATE TABLE SRRMS_DB.dbo.Devices(
+	D_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Class_Description varchar(50),
+	C_DeviceID char(10) UNIQUE,
+)
+GO
+
+CREATE TABLE SRRMS_DB.dbo.Student_RFID(
+	S_UID uniqueidentifier NOT NULL default newid() PRIMARY KEY,
+	Card_ID varchar(255) UNIQUE
+)
+GO
+
+CREATE TABLE SRRMS_DB.dbo.Student_Info(
 	SI_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	S_UID uniqueidentifier,
 	FOREIGN KEY (S_UID) REFERENCES Student_RFID(S_UID),
@@ -12,13 +26,7 @@ CREATE TABLE Student_Info(
 )
 GO
 
-CREATE TABLE Student_RFID(
-	S_UID uniqueidentifier NOT NULL default newid() PRIMARY KEY,
-	Card_ID varchar(255)
-)
-GO
-
-CREATE TABLE Class_Info(
+CREATE TABLE SRRMS_DB.dbo.Class_Info(
 	C_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	C_Name varchar(80),
 	C_Description varchar(255),
@@ -27,50 +35,48 @@ CREATE TABLE Class_Info(
 )
 GO
 
-CREATE TABLE Location_Override(
+CREATE TABLE SRRMS_DB.dbo.Location_Override(
 	LO_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	C_ID int NOT NULL,
 	FOREIGN KEY (C_ID) REFERENCES Class_Info(C_ID),
 	Date_Day date NOT NULL,
-	Period tinyint NOT NULL,
+	Period_Num tinyint NOT NULL,
 	D_ID int NOT NULL,
 	FOREIGN KEY (D_ID) REFERENCES Devices(D_ID),
 )
 GO
 
-CREATE TABLE Locations(
+CREATE TABLE SRRMS_DB.dbo.Locations(
 	L_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	C_ID int NOT NULL,
 	FOREIGN KEY (C_ID) REFERENCES Class_Info(C_ID),
 	Date_Day date NOT NULL,
-	Period tinyint NOT NULL,
+	Period_Num tinyint NOT NULL,
 	D_ID int NOT NULL,
 	FOREIGN KEY (D_ID) REFERENCES Devices(D_ID),
 )
 GO
 
-CREATE TABLE Devices(
-	D_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	Class_Description varchar(50),
-	C_DeviceID char(10)
-)
-GO
 
-CREATE TABLE Attendence(
+CREATE TABLE SRRMS_DB.dbo.Attendence(
 	A_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	SI_ID int NOT NULL,
 	FOREIGN KEY (SI_ID) REFERENCES  Student_Info(SI_ID),
+	A_Day date,
+	A_Period_Num tinyint NOT NULL,
+	A_Attended bit NOT NULL,
+	C_Description varchar(255),
 )
 GO
 
-CREATE TABLE Times(
+CREATE TABLE SRRMS_DB.dbo.Times(
 	T_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	Period_Num tinyint NOT NULL,
+	Period_Num tinyint NOT NULL UNIQUE,
 	Period_Time time NOT NULL,
 )
 GO
 
-CREATE TABLE Time_Override(
+CREATE TABLE SRRMS_DB.dbo.Time_Override(
 	TO_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Period_Num tinyint NOT NULL,
 	FOREIGN KEY(Period_Num) REFERENCES Times(Period_Num),
@@ -78,7 +84,7 @@ CREATE TABLE Time_Override(
 )
 GO
 
-CREATE TABLE Data_Dump(
+CREATE TABLE SRRMS_DB.dbo.Data_Dump(
 	DD_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	C_DeviceID char(10),
 	FOREIGN KEY(C_DeviceID) REFERENCES Devices(C_DeviceID),
