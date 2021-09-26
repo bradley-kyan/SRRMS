ff780f119fb0bbc2fa01e199a280b60311a7024f
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2021-09-26 22:00:49 +1300

    Update UID.ino
    ~ Fixed code formatting

--- UID/UID.ino
+++ UID/UID.ino
@@ -7,15 +7,14 @@
 #include <Dns.h>
 
 
-#define RST_PIN         7          // Configurable, see typical pin layout above
-#define SS_PIN          6         // Configurable, see typical pin layout above
+#define RST_PIN         7
+#define SS_PIN          6
 
 String DeviceID = "i19ahbaj16";
 int errorDelay = 1000;
 int successDelay = 200;
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+//Mac adress of the device
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 EthernetClient client;
@@ -25,7 +24,7 @@ DNSClient dnsC;
 EthernetUDP Udp;
 unsigned int localPort = 8888;
 IPAddress timeServer;
-IPAddress dnServer(8,8,8,8);
+IPAddress dnServer(8, 8, 8, 8);
 
 String currentTime;
 const int timeZone = 12;
@@ -34,52 +33,52 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
 
-Serial.begin(9600);   // Initialize serial communications with the PC
+  Serial.begin(9600);   // Initialize serial communications with the PC
 
   SPI.begin();    // Init SPI bus
   SPI.setClockDivider(SPI_CLOCK_DIV16);
-  
+
   mfrc522.PCD_Init();   // Init MFRC522
   Serial.println("Initalized MFRC522");
   Ethernet.begin(mac);
-    Serial.println("Connection Successful");
+  Serial.println("Connection Successful");
   delay(200);
   dnsC.begin(dnServer);
-  dnsC.getHostByName("time-a-g.nist.gov",timeServer);
+  dnsC.getHostByName("time-a-g.nist.gov", timeServer);
   Serial.println("Got NTP IP");
   delay(200);
   Serial.print("Updating Time.");
   delay(500);
-  
+
   Serial.println();
   Udp.begin(localPort);
   setSyncProvider(getNtpTime);
 }
 
-  String getID(){
-    if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
-      return "";
-    }
-    String uidString =  String(mfrc522.uid.uidByte[0]);
-    uidString +=  String(mfrc522.uid.uidByte[1]);
-    uidString +=  String(mfrc522.uid.uidByte[2]);
-    uidString +=  String(mfrc522.uid.uidByte[3]);
-    mfrc522.PICC_HaltA(); // Stop reading
-    return uidString;
+String getID() {
+  if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
+    return "";
   }
-  
+  String uidString =  String(mfrc522.uid.uidByte[0]);
+  uidString +=  String(mfrc522.uid.uidByte[1]);
+  uidString +=  String(mfrc522.uid.uidByte[2]);
+  uidString +=  String(mfrc522.uid.uidByte[3]);
+  mfrc522.PICC_HaltA(); // Stop reading
+  return uidString;
+}
 
-class tones{
+
+class tones {
   public:
-  void error(){
+    void error() {
       tone(5, 2600, errorDelay); //error tone to D5
       delay(errorDelay);
-  }
-  void success(){
+    }
+    void success() {
       tone(5, 3300, successDelay); //Success tone to D5
       delay(successDelay);
       tone(5, 3400, successDelay);
-  }
+    }
 };
 
 /*-------- NTP code ----------*/
@@ -128,7 +127,7 @@ void sendNTPpacket(IPAddress &address)
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
   // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
+  // you can send a packet requesting a timestamp:
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
@@ -137,50 +136,50 @@ void sendNTPpacket(IPAddress &address)
 void loop() {
   tones Tones;
   Ethernet.maintain();
-  currentTime = (String)year() + "-" + (String)month() + "-" + (String)day()+ " " +(String)hour() + ":" + (String)minute() + ":" + (String)second(); //Update current time to be sent
+  currentTime = (String)year() + "-" + (String)month() + "-" + (String)day() + " " + (String)hour() + ":" + (String)minute() + ":" + (String)second(); //Update current time to be sent
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-  else{
+  else {
     String uid = getID();
-    if(lastUid == uid){ //check if current card has the same uid as last scanned card
+    if (lastUid == uid) { //check if current card has the same uid as last scanned card
       Serial.println("Card Already Scanned");
       Tones.error();
     }
-    else if(uid == "" || uid == " "){ //Check if UID is empty
+    else if (uid == "" || uid == " ") { //Check if UID is empty
       Tones.error();
     }
-    else{
+    else {
       Serial.print("Card detected, UID: "); Serial.println(uid);
       lastUid = uid; //save current card to local memory
       int i = 0;
-      retry:
-      if(client.connect(IPAddress(192,168,1,75), 29882)){
+retry:
+      if (client.connect(IPAddress(192, 168, 1, 75), 29882)) {
         Serial.println("Client connected");
-        String data = DeviceID+"?"+uid+"?"+currentTime+"<EOF>";
+        String data = DeviceID + "?" + uid + "?" + currentTime + "<EOF>";
         client.print(data);
-        Serial.println(data+"\n");
-          String rData;
-          while(client.available()){
-              rData += (char)client.read();
-              }
-          if (!client.available()) {
-            Serial.println(rData + "\n");
-             Tones.success();
-              client.stop();
-              return; 
-          }
+        Serial.println(data + "\n");
+        String rData;
+        while (client.available()) {
+          rData += (char)client.read();
         }
-      else{
-        if(i == 0){
+        if (!client.available()) {
+          Serial.println(rData + "\n");
+          Tones.success();
+          client.stop();
+          return;
+        }
+      }
+      else {
+        if (i == 0) {
           goto retry;
           i++;
         }
-        else{
+        else {
           Tones.error();
-          }
-        }  
+        }
+      }
     }
   }
 }
